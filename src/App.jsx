02a6de209b1f2699b57d04ee273ad5d0d7df4ccf
nashie1004 <naocenar@gmail.com>
2023-03-24 15:27:15 +0800

    onscroll animate added

--- src/App.jsx
+++ src/App.jsx
@@ -12,21 +12,35 @@ function App() {
     const observer = new IntersectionObserver((entries, observer) => {
       entries.forEach(entry => {
         if (entry.isIntersecting || entry.isVisible){
-          entry.target.classList.add('slide-animation')
+          entry.target.classList.add('animate')
         } else {
-          entry.target.classList.remove('slide-animation')
+          entry.target.classList.remove('animate')
         }
       }) 
     }, {
       root: null,
       rootMargin: '0px',
-      threshold: .3
+      threshold: .1
     });
 
-    const sectionsDiv = sectionsRef.current.childNodes[1].children;
-    [...sectionsDiv].forEach(div => {
-      observer.observe(div);
-    })
+    const sectionsDiv = [...sectionsRef.current.childNodes[1].children];
+    const array = [];
+    
+    array.push(
+      sectionsDiv[1].childNodes[0],
+      sectionsDiv[1].childNodes[1],
+      sectionsDiv[2].childNodes[0],
+      sectionsDiv[2].childNodes[1],
+      sectionsDiv[3].childNodes[0],
+      sectionsDiv[3].childNodes[1].childNodes[0],
+      sectionsDiv[3].childNodes[1].childNodes[1],
+      sectionsDiv[3].childNodes[1].childNodes[2],
+      sectionsDiv[4].childNodes[0],
+      sectionsDiv[4].childNodes[1]
+    )
+
+    array.forEach(div => div.classList.add('no-animate'))
+    array.forEach(div => observer.observe(div))
 
   }, [])
 
